022feaad4d26c0addde0ca2d01d39e1614ed71aa
Esa Firman <esafirm21@gmail.com>
2020-11-01 13:49:20 +0700

    migrate core using kotlinx serialization

--- skrape-core/src/main/kotlin/nolambda/skrape/serialization/JsonPageSerializer.kt
+++ skrape-core/src/main/kotlin/nolambda/skrape/serialization/JsonPageSerializer.kt
@@ -1,21 +1,19 @@
 package nolambda.skrape.serialization
 
-import com.github.salomonbrys.kotson.get
-import com.google.gson.Gson
-import com.google.gson.JsonArray
-import com.google.gson.JsonElement
-import com.google.gson.JsonObject
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.*
 import nolambda.skrape.nodes.*
 
-class JsonPageSerializer(private val gson: Gson = Gson()) : PageSerializer<String> {
+class JsonPageSerializer(private val json: Json = Json { encodeDefaults = true }) : PageSerializer<String> {
 
     override fun serialize(page: Page): String {
-        return gson.toJson(page.evaluate())
+        return json.encodeToString(page.evaluate())
     }
 
     override fun deserialize(target: String): Page {
-        val jsonObject = gson.fromJson(target, JsonObject::class.java)
-        val pageInfo = gson.fromJson(jsonObject[PageSerializer.KEY_PAGE_INFO], PageInfo::class.java)
+        val jsonObject = json.decodeFromString<JsonObject>(target)
+        val pageInfo = json.decodeFromJsonElement<PageInfo>(jsonObject[PageSerializer.KEY_PAGE_INFO]!!)
 
         return Page(pageInfo = pageInfo, name = jsonObject.getName()) {
             children.addAll(createChildrenFromJsonArray(jsonObject.getChildArray()))
@@ -30,19 +28,20 @@ class JsonPageSerializer(private val gson: Gson = Gson()) : PageSerializer<Strin
         else -> throw IllegalArgumentException("Not a valid page JSON!")
     }
 
-    private fun createElementFromJson(json: JsonElement): SkrapeElemenet {
-        val clazz = mapTypeToClass(json[PageSerializer.KEY_TYPE].asString)
+    private fun createElementFromJson(element: JsonElement): SkrapeElemenet {
+        val type = element.jsonObject[PageSerializer.KEY_TYPE]!!.jsonPrimitive.content
+        val clazz = mapTypeToClass(type)
         return if (ParentElement::class.java.isAssignableFrom(clazz)) {
-            createParentElement(clazz, json)
+            createParentElement(clazz, element)
         } else {
-            json.fromJson(clazz)
+            json.decodeFromJsonElement(element)
         }
     }
 
     private fun <T : SkrapeElemenet> createParentElement(clazz: Class<T>, content: JsonElement): SkrapeElemenet {
         return when (clazz) {
             Query::class.java -> Query(
-                content[PageSerializer.KEY_SELECTOR].asString,
+                content.jsonObject[PageSerializer.KEY_SELECTOR]!!.jsonPrimitive.content,
                 content.getName(),
                 content.createBody()
             )
@@ -55,25 +54,25 @@ class JsonPageSerializer(private val gson: Gson = Gson()) : PageSerializer<Strin
     }
 
     private fun createChildrenFromJsonArray(children: JsonArray): List<SkrapeElemenet> {
-        return children.map { child ->
+        return children.map { child: JsonElement ->
             createElementFromJson(child)
         }
     }
 
-    private fun JsonElement.getChildArray() =
-        get(PageSerializer.KEY_PAGE_CHILDREN).asJsonArray
+    private fun JsonElement.getChildArray(): JsonArray {
+        return when (this) {
+            is JsonObject -> get(PageSerializer.KEY_PAGE_CHILDREN) as JsonArray
+            else -> throw IllegalStateException("Element is not an object: $this")
+        }
+    }
 
-    private fun JsonElement.getName() =
-        get(PageSerializer.KEY_NAME).asString
+    private fun JsonElement.getName() = when (this) {
+        is JsonObject -> get(PageSerializer.KEY_NAME)!!.jsonPrimitive.content
+        else -> throw IllegalStateException("Element is not an object: $this")
+    }
 
     private fun JsonElement.createBody(): ElementBody {
         val json = this
         return { children.addAll(createChildrenFromJsonArray(json.getChildArray())) }
     }
-
-    private fun <T : SkrapeElemenet> JsonElement.fromJson(clazz: Class<T>): T {
-        return gson.fromJson(this, clazz)
-    }
-
-
 }
--- skrape-core/src/test/kotlin/nolamda/skrape/QuerySkapeResultSpec.kt
+++ skrape-core/src/test/kotlin/nolamda/skrape/QuerySkapeResultSpec.kt
@@ -1,34 +1,30 @@
 package nolamda.skrape
 
-import com.github.salomonbrys.kotson.get
-import com.github.salomonbrys.kotson.jsonArray
-import com.github.salomonbrys.kotson.jsonObject
-import com.google.gson.Gson
-import com.google.gson.JsonArray
-import com.google.gson.JsonObject
 import io.kotlintest.matchers.shouldBe
 import io.kotlintest.specs.StringSpec
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.*
 import nolambda.skrape.result.QuerySkrapeResult
 
 class QuerySkapeResultSpec : StringSpec({
     val result = QuerySkrapeResult(
-        jsonArray(
-            jsonObject(
-                "title" to "How to kill a dragon",
-                "rating" to 5
-            ),
-            jsonObject(
-                "title" to "How to kill a meme",
-                "rating" to 10
-            )
-        )
+        JsonArray(listOf(
+            JsonObject(mapOf(
+                "title" to JsonPrimitive("How to kill a dragon"),
+                "rating" to JsonPrimitive(5)
+            )),
+            JsonObject(mapOf(
+                "title" to JsonPrimitive("How to kill a meme"),
+                "rating" to JsonPrimitive(10)
+            ))
+        ))
     )
 
-    val toJsonObj = { string: String -> Gson().fromJson(string, JsonObject::class.java) }
-    val toJsonArray = { string: String -> Gson().fromJson(string, JsonArray::class.java) }
+    val toJsonObj = { string: String -> Json.decodeFromString<JsonObject>(string) }
+    val toJsonArray = { string: String -> Json.decodeFromString<JsonArray>(string) }
 
     "it should have the right count" {
-        toJsonObj(result.count).get("count").asInt shouldBe 2
+        toJsonObj(result.count).get("count")?.jsonPrimitive?.int shouldBe 2
     }
 
     "it should have the json result" {
@@ -38,12 +34,12 @@ class QuerySkapeResultSpec : StringSpec({
     "it should get the right index" {
         val firstIndex = result.at(1)
         val obj = toJsonObj.invoke(firstIndex)
-        obj.get("title").asString.contains("meme") shouldBe true
+        obj.get("title").toString().contains("meme") shouldBe true
     }
 
     "it should get the right item" {
         val memeItem = result.find("title=meme")
         val obj = toJsonArray.invoke(memeItem)
-        obj.first().get("title").asString.contains("meme") shouldBe true
+        obj.first().jsonObject["title"].toString().contains("meme") shouldBe true
     }
 })
